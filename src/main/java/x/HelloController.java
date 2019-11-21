de3a22c457ac6ba7c63c75cd21662f532216271c
HunghhDev <hunghh.dev@gmail.com>
2019-11-21 15:02:14 +0700

    first commit

---
+++ src/main/java/x/HelloController.java
@@ -0,0 +1,17 @@
+package x;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("")
+@Controller
+public class HelloController {
+    @GetMapping({"/", "/hello"})
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
