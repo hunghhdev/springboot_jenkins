94d56a3c6d6d158a6db46113c377aa4a306af9e3
HungHHDev <hunghh.dev@gmail.com>
2019-11-22 08:50:26 +0700

    fix syntax

--- src/main/java/x/HelloController.java
+++ src/main/java/x/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
     @GetMapping({"/", "/hello"})
     public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
         model.addAttribute("name", name);
-        if(true) int x = 0;
+        if(true){ int x = 0;}
         return "hello";
     }
 }
